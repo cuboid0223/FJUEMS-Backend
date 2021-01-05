73e5875ad937305ed0be37e4992307fd43f01bfb
cuboid0223 <59163273+cuboid0223@users.noreply.github.com>
2021-01-05 09:14:25 +0800

    cancel event

---
+++ cancelJoinEvent.php
@@ -0,0 +1,47 @@
+<?php
+// CORS
+// 設置允許其他域名訪問
+header('Access-Control-Allow-Origin:*');  
+// 設置允許的響應類型 
+header('Access-Control-Allow-Methods:POST, GET');  
+//設置允許的響應頭 
+header('Access-Control-Allow-Headers:x-requested-with,content-type');
+
+include "db_connect.php";
+
+$postData = file_get_contents("php://input"); 
+$request = json_decode($postData);
+if(empty($request)){
+    //echo 'no request';
+}else{
+    print_r($request);
+}
+
+if(!empty($_GET['eventID']) and !empty($_GET['userID'])){
+    $eve_id = $_GET['eventID'];
+    $user_id = $_GET['userID'];
+    $find_sql = 
+    "
+    SELECT * FROM USERS_EVENTS WHERE user_id = '$user_id' AND eve_id='$eve_id'
+    ";
+    $sql = 
+    "
+    DELETE FROM `USERS_EVENTS` WHERE `USERS_EVENTS`.`user_id` = $user_id AND `USERS_EVENTS`.`eve_id` = $eve_id;
+    ";
+    $statement = $connection -> prepare($find_sql);// ???
+    $statement -> execute();// ???
+    $userJoinEvent = $statement -> fetch(PDO::FETCH_ASSOC);// ???
+    if($userJoinEvent){
+        $statement = $connection -> prepare($sql);// ???
+        $statement -> execute();// ???
+        echo "取消成功";
+    }else{
+        echo "未報名";
+    }
+}
+
+
+
+
+
+?>
